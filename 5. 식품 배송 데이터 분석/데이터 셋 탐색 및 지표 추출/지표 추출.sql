f16c6fc5006829f9900740133227dc1e7eee2664
JunSeokCheon <goosw775@naver.com>
2022-07-26 04:06:52 +0000

    SQL로 배우는 데이터 전처리 분석

--- 5. 식품 배송 데이터 분석/데이터 셋 탐색 및 지표 추출/지표 추출.sql
+++ 5. 식품 배송 데이터 분석/데이터 셋 탐색 및 지표 추출/지표 추출.sql
@@ -86,3 +86,54 @@ FROM INSTACART.ORDER_PRODUCTS__PRIOR;
 -- 전체 주문 건수를 구매자 수로 나누어 인당 평균 주문 건수를 구할 수 있습니다.
 SELECT COUNT(DISTINCT ORDER_ID) / COUNT(DISTINCT USER_ID) AVG_COUNT
 FROM INSTACART.ORDERS;
+
+-- 9) 재구매율이 가장 높은 상품 10개
+-- 재구매율이 가장 높은 상품을 구하기 위해서 먼저 상품별로 재구매율을 계산한 뒤, 재구매율을 기준으로 랭크를 계산합니다. 이후, 원하는 랭크 값으로 조건을 생성해서 상품을 추출합니다.
+-- a) 상품별 재구매율 계산
+-- 상품 번호로 데이터를 그룹핑한 후, ORDER_PRODUCTS__PRIOR 테이블의 REORDERED 컬럼을 활용해 재구매 수를 전체 구매 수로 나누어 재구매율을 계산합니다.
+SELECT PRODUCT_ID,
+SUM(CASE WHEN REORDERED = 1 THEN 1 ELSE 0 END)/COUNT(*) REORDER_RATIO
+FROM INSTACART.ORDER_PRODUCTS__PRIOR
+GROUP BY 1;
+
+-- b) 재구매율로 순위열 생성하기
+-- 앞에서 생성한 상품별 재구매율로 순위를 구합니다. 앞의 결과를 SUBQUERY로 생성하고, SELECT 문에서 순위문(ROW_NUMBER, RANK, DENSE_RANK)을 사용해서 순위 열을 생성합니다.
+SELECT *,
+ROW_NUMBER() OVER(ORDER BY REORDER_RATIO DESC) RNK
+FROM
+(SELECT PRODUCT_ID,
+SUM(CASE WHEN REORDERED = 1 THEN 1 ELSE 0 END)/COUNT(*) REORDER_RATIO
+FROM INSTACART.ORDER_PRODUCTS__PRIOR
+GROUP BY 1) A;
+
+-- c) Top 10 상품 추출
+-- 상품별로 재구매율을 계산하고, 재구매율 순위 열을 생성했습니다. 생성한 순위 열에서 1-10 데이터를 추출하면 됩니다.
+SELECT *
+FROM
+(SELECT *,
+ROW_NUMBER() OVER(ORDER BY REORDER_RATIO DESC) RNK
+FROM
+(SELECT PRODUCT_ID,
+SUM(CASE WHEN REORDERED = 1 THEN 1 ELSE 0 END)/COUNT(*) REORDER_RATIO
+FROM INSTACART.ORDER_PRODUCTS__PRIOR
+GROUP BY 1) A) BASE
+WHERE RNK <= 10;
+
+-- 10) Department별 재구매율이 가장 높은 상품 10개
+-- Department별로 재구매율이 높은 상품을 추출할려면, Department/상품별 그룹핑하여 재구매율을 계산해야 합니다.
+-- 앞 9번 예제에서 추가된 내용은 ORDER_PRODUCTS__PRIOR에 PRODUCTS 테이블을 조인해 DEPARTMENT를 가져와야 한다는 점과 DEPARTMENT로 PARTITION을 생성해 순위를 매겨야 한다는 것을 유의하고 수행해보겠습니다.
+SELECT *
+FROM
+(SELECT *,
+ROW_NUMBER() OVER(ORDER BY REORDER_RATIO DESC) RNK
+FROM
+(SELECT C.DEPARTMENT,
+A.PRODUCT_ID,
+SUM(CASE WHEN REORDERED = 1 THEN 1 ELSE 0 END)/COUNT(*) REORDER_RATIO
+FROM INSTACART.ORDER_PRODUCTS__PRIOR A
+LEFT JOIN INSTACART.PRODUCTS B
+ON A.PRODUCT_ID = B.PRODUCT_ID
+LEFT JOIN INSTACART.DEPARTMENTS C
+ON B.DEPARTMENT_ID = C.DEPARTMENT_ID
+GROUP BY 1, 2) A) A
+WHERE RNK <= 10;
